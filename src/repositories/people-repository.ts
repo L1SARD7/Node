95253c8debeb5e2061e371fa3e87b63c75f0a75e
L1SARD7 <arthem.klimchuk7@gmail.com>
2025-02-06 00:19:04 +0200

    added layered architecture for people

---
+++ src/repositories/people-repository.ts
@@ -0,0 +1,69 @@
+import { db } from "../db/db"
+
+
+export const PeopleRepository = {
+    GetPeople(name: string, sex: string, age: number, isOld: string) {
+        let CorrectInputOld = true
+        let SortedPeople = db.people
+            if (name) {
+                SortedPeople = db.people.filter(p => p.name.indexOf(name) > -1)
+            }
+            if (sex)
+                SortedPeople = SortedPeople.filter(p => p.sex === sex)
+            if (isOld) {
+                switch (isOld) {
+                    case "true": SortedPeople = SortedPeople.filter(p => p.isOld === true)
+                        break
+                    case "false": SortedPeople = SortedPeople.filter(p => p.isOld === false)
+                        break
+                    default: CorrectInputOld = false
+                        break
+                }
+            }
+        if (CorrectInputOld) {
+            return(SortedPeople)
+        } else {
+            return false
+        }
+    },
+
+    DeletePeople (name: string) {
+        let Founded_people = db.people.find(p => p.name === name)
+            if (Founded_people) {
+                db.people = db.people.filter(p => p.name !== name)
+                return true
+            }
+            else
+                return false
+    },
+
+    CreateNewPerson (name: string, sex: string, age: number, isOld: boolean) {
+                let AddedPerson = {
+                    name: name,
+                    sex: sex,
+                    age: age,
+                    isOld: isOld
+                }
+                db.people.push(AddedPerson)
+                return AddedPerson
+    },
+
+    UpdatePerson (OldName: string, NewName: string, sex: string, age: number, isOld: boolean) {
+    let SelectedPerson = db.people.find(p => p.name === OldName)
+        if ((!SelectedPerson) || (!NewName && !age && !sex && (isOld === undefined))) {
+            return false
+        }
+        if (NewName) {
+            SelectedPerson.name = NewName
+        } if (age) {
+            SelectedPerson.age = age
+        }
+        if (sex) {
+            SelectedPerson.sex = sex
+        }
+        if (isOld !== undefined) {
+            SelectedPerson.isOld = isOld
+        }
+        return SelectedPerson    
+    }
+}
